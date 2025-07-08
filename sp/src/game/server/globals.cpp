5e3f740f0797e43d08f0747a8cbce6e6245843a9
Iliketocreatestuff2001 <Zombiegoon67@gmail.com>
2025-07-08 19:23:05 +0200

    Update globals.cpp

--- sp/src/game/server/globals.cpp
+++ sp/src/game/server/globals.cpp
@@ -26,7 +26,6 @@ Vector			g_vecAttackDir;
 int				g_iSkillLevel;
 bool			g_fGameOver;
 
-ConVar sv_custom_gamemode("sv_custom_gamemode", "tactical_combat", FCVAR_GAMEDLL | FCVAR_HIDDEN);
 ConVar sv_enable_hitscan_weapons("sv_enable_hitscan_weapons", "1", FCVAR_GAMEDLL | FCVAR_HIDDEN);
 
 ConVar sk_bullet_speed("sk_bullet_speed", "15000", FCVAR_GAMEDLL | FCVAR_REPLICATED);
